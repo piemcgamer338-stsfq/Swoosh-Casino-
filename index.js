ced7ed3046be051c6a67b0a98dfe2bdf7516c614
piemcgamer338-stsfq <piemcgamer338@gmail.com>
2026-07-23 14:31:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
 require("dotenv").config();
 
-const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js");
+const {
+    Client,
+    GatewayIntentBits,
+    Partials,
+    Collection
+} = require("discord.js");
+
 
 const loadCommands = require("./handlers/commandHandler");
 const loadEvents = require("./handlers/eventHandler");
+const loadInteractions = require("./handlers/interactionHandler");
+
 
 const client = new Client({
+
     intents: [
+
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.MessageContent
+
     ],
+
     partials: [
+
         Partials.Channel,
         Partials.Message,
         Partials.User
+
     ]
+
 });
 
+
+// Commands
 client.commands = new Collection();
 
+
+// Interactions
+client.interactions = new Map();
+
+
+// Load systems
 loadCommands(client);
+loadInteractions(client);
 loadEvents(client);
 
+
+// Login
 client.login(process.env.DISCORD_TOKEN);
