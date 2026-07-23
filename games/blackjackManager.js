679768827a75fc05e1436da4aca8c0fb393b5eca
piemcgamer338-stsfq <piemcgamer338@gmail.com>
2026-07-23 18:36:50 +0530

    Update blackjackManager.js

--- games/blackjackManager.js
+++ games/blackjackManager.js
@@ -1,36 +1,3 @@
-class BlackjackManager {
+const blackjackGames = new Map();
 
-    constructor() {
-        this.games = new Map();
-    }
-
-    create(messageId, game) {
-        this.games.set(messageId, game);
-    }
-
-    get(messageId) {
-        return this.games.get(messageId);
-    }
-
-    has(messageId) {
-        return this.games.has(messageId);
-    }
-
-    delete(messageId) {
-        return this.games.delete(messageId);
-    }
-
-    update(messageId, data) {
-
-        const game = this.games.get(messageId);
-
-        if (!game) return;
-
-        Object.assign(game, data);
-
-        this.games.set(messageId, game);
-    }
-
-}
-
-module.exports = new BlackjackManager();
+module.exports = blackjackGames;
