6e785a0bce336b201f2d72d06d4499ec8b43a011
piemcgamer338-stsfq <piemcgamer338@gmail.com>
2026-07-23 17:05:18 +0530

    Update bj_stand.js

--- interactions/bj_stand.js
+++ interactions/bj_stand.js
@@ -1,12 +1,28 @@
+const blackjackGames = require("../games/blackjackManager");
+
 module.exports = {
 
     customId: "bj_stand",
 
     async execute(interaction) {
 
+        const game = blackjackGames.get(interaction.message.id);
+
+        if (!game)
+            return interaction.reply({
+                content:"❌ Game expired.",
+                ephemeral:true
+            });
+
+        if (interaction.user.id !== game.userId)
+            return interaction.reply({
+                content:"❌ This isn't your game.",
+                ephemeral:true
+            });
+
         await interaction.reply({
-            content: "🛑 Stand button clicked!",
-            ephemeral: true
+            content:
+            `🛑 You stood.\n\nYour cards:\n${game.player.map(c=>`${c.value}${c.suit}`).join(" ")}`
         });
 
     }
