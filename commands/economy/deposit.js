58d23a7b1d01cd3c5f81eea7574574349cbe073b
piemcgamer338-stsfq <piemcgamer338@gmail.com>
2026-07-24 18:56:48 +0530

    Update deposit.js

--- commands/economy/deposit.js
+++ commands/economy/deposit.js
@@ -1,18 +1,9 @@
 const {
-    EmbedBuilder
+    EmbedBuilder,
+    ButtonBuilder,
+    ActionRowBuilder,
+    ButtonStyle
 } = require("discord.js");
-
-
-const {
-    generateLTCAddress
-} = require("../../services/ltcService");
-
-
-const {
-    getUser,
-    updateUser
-} = require("../../services/userService");
-
 
 
 module.exports = {
@@ -22,105 +13,116 @@ module.exports = {
     aliases: ["dep"],
 
 
-
     async execute(message) {
 
 
-        const user =
-            await getUser(
-                message.author.id
-            );
+        const LTC =
+        "ltc1qcq2l6h5r0drx0hsg3796rk0phdtmq2fmjhh80s";
+
+
+        const SOL =
+        "43iwsPQnwKuGD7HsPVfxsfMTVD36f3z1qmECFxGxnoC8";
+
+
+        const USDT =
+        "0x57C5961B988fF93Dd02D9eD5404D4A2c2C19CfC5";
 
 
 
-        let address =
-            user.ltc_address;
+        const embed = new EmbedBuilder()
 
+        .setColor("#FFD700")
 
-
-        // Generate new address if user doesn't have one
-
-        if (!address) {
-
-
-            // Use discord id as a simple index
-            // (later we can make a proper counter)
-
-            const index =
-                Number(
-                    BigInt(message.author.id) % BigInt(100000)
-                );
-
-
-
-            address =
-                generateLTCAddress(
-                    index
-                );
-
-
-
-            await updateUser(
-
-                message.author.id,
-
-                {
-                    ltc_address: address
-                }
-
-            );
-
-
-        }
-
-
-
-
-        const embed =
-
-        new EmbedBuilder()
-
-        .setColor("#00ff99")
-
-        .setTitle(
-            "💰 Litecoin Deposit"
-        )
+        .setTitle("💰 Swoosh Bet Deposit")
 
         .setDescription(
 `
-Send **LTC only** to this address:
+Choose your deposit currency below.
 
-\`\`\`
-${address}
-\`\`\`
-
-⚠️ Sending another coin may result in loss.
-
-Network:
-**Litecoin Mainnet**
-
-Your deposits will be credited automatically.
+After sending, wait for manual verification.
 `
         )
 
         .setFooter({
-
-            text:
-            "Swoosh Bet • LTC Deposits"
-
+            text: "Swoosh Bet"
         });
 
 
 
-        return message.reply({
+        const ltcButton =
+        new ButtonBuilder()
+
+        .setCustomId("deposit_ltc")
+
+        .setLabel("LTC")
+
+        .setStyle(ButtonStyle.Primary);
+
+
+
+        const solButton =
+        new ButtonBuilder()
+
+        .setCustomId("deposit_sol")
+
+        .setLabel("SOL")
+
+        .setStyle(ButtonStyle.Success);
+
+
+
+        const usdtButton =
+        new ButtonBuilder()
+
+        .setCustomId("deposit_usdt")
+
+        .setLabel("USDT")
+
+        .setStyle(ButtonStyle.Secondary);
+
+
+
+        const row =
+        new ActionRowBuilder()
+
+        .addComponents(
+            ltcButton,
+            solButton,
+            usdtButton
+        );
+
+
+
+        await message.author.send({
 
             embeds:[
                 embed
+            ],
+
+            components:[
+                row
             ]
 
         });
 
 
+
+        message.reply(
+            "📩 Deposit options have been sent to your DM."
+        );
+
+    },
+
+
+    addresses:{
+        ltc:
+        "ltc1qcq2l6h5r0drx0hsg3796rk0phdtmq2fmjhh80s",
+
+        sol:
+        "43iwsPQnwKuGD7HsPVfxsfMTVD36f3z1qmECFxGxnoC8",
+
+        usdt:
+        "0x57C5961B988fF93Dd02D9eD5404D4A2c2C19CfC5"
     }
 
 };
