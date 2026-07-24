1c672d34bf1df698093c75e4d3d8146dda8cf408
piemcgamer338-stsfq <piemcgamer338@gmail.com>
2026-07-24 16:51:14 +0530

    Update blackjackRenderer.js

--- utils/blackjackRenderer.js
+++ utils/blackjackRenderer.js
@@ -1,87 +1,165 @@
+// utils/blackjackRenderer.js
+
 const {
     EmbedBuilder,
     AttachmentBuilder
 } = require("discord.js");
 
-const { createCanvas } = require("@napi-rs/canvas");
-const { handValue } = require("./blackjackLogic");
+const path = require("path");
 
-function cardText(card) {
-    return `${card.value}${card.suit}`;
-}
 
-function tableImage() {
+function cardFile(card) {
 
-    const canvas = createCanvas(1200, 700);
-    const ctx = canvas.getContext("2d");
-
-    // Table
-    ctx.fillStyle = "#0c4d1f";
-    ctx.fillRect(0, 0, 1200, 700);
-
-    // Border
-    ctx.strokeStyle = "#d4af37";
-    ctx.lineWidth = 12;
-    ctx.strokeRect(20, 20, 1160, 660);
-
-    // Dealer zone
-    ctx.fillStyle = "#ffffff";
-    ctx.font = "bold 42px Arial";
-    ctx.fillText("DEALER", 70, 80);
-
-    // Player zone
-    ctx.fillText("PLAYER", 70, 400);
-
-    return new AttachmentBuilder(
-        canvas.toBuffer("image/png"),
-        {
-            name: "blackjack.png"
-        }
+    return path.join(
+        process.cwd(),
+        "assets",
+        "blackjack",
+        `${card}.png`
     );
 
 }
 
+
+
 function gameEmbed(game, revealDealer = false) {
 
-    const dealerCards = revealDealer
-        ? game.dealer.map(cardText).join(" ")
-        : `?? ${cardText(game.dealer[1])}`;
 
-    const playerCards =
-        game.player.map(cardText).join(" ");
+    const files = [];
 
-    const dealerValue =
-        revealDealer
-            ? handValue(game.dealer)
-            : "?";
+    const playerImages = [];
+    const dealerImages = [];
 
-    const playerValue =
-        handValue(game.player);
+
+
+    // PLAYER CARDS
+
+    for (const card of game.player) {
+
+        const file = new AttachmentBuilder(
+            cardFile(card)
+        );
+
+        files.push(file);
+
+        playerImages.push(
+            `attachment://${card}.png`
+        );
+
+    }
+
+
+
+    // DEALER CARDS
+
+    if (revealDealer) {
+
+
+        for (const card of game.dealer) {
+
+            const file = new AttachmentBuilder(
+                cardFile(card)
+            );
+
+            files.push(file);
+
+            dealerImages.push(
+                `attachment://${card}.png`
+            );
+
+        }
+
+
+    } else {
+
+
+        const hidden = new AttachmentBuilder(
+
+            cardFile("BACK")
+
+        );
+
+        files.push(hidden);
+
+
+        dealerImages.push(
+            "attachment://BACK.png"
+        );
+
+
+        dealerImages.push(
+            `attachment://${game.dealer[1]}.png`
+        );
+
+
+    }
+
+
+
+
 
     const embed = new EmbedBuilder()
-        .setColor("#2ecc71")
-        .setTitle("🃏 Blackjack")
-        .setDescription(
-`## Bet
-💰 **${game.bet}**
 
-### Dealer
-Cards: ${dealerCards}
-Value: ${dealerValue}
+        .setColor("#0b6623")
 
-### Player
-Cards: ${playerCards}
-Value: ${playerValue}`
+        .setTitle(
+            "🃏 Blackjack"
         )
-        .setImage("attachment://blackjack.png");
+
+
+        .addFields(
+
+            {
+                name:"Dealer",
+
+                value:
+                dealerImages
+                .map(x=>`🂠 ${x}`)
+                .join("\n")
+            },
+
+
+            {
+
+                name:"Player",
+
+                value:
+                playerImages
+                .join("\n")
+
+            }
+
+        )
+
+
+        .setDescription(
+
+`
+💰 Bet: **${game.bet} Points**
+
+Hit or Stand?
+`
+
+        );
+
+
 
     return {
-        embeds: [embed],
-        files: [tableImage()]
+
+        embeds:[
+            embed
+        ],
+
+        files
+
     };
+
 
 }
 
+
+
 module.exports = {
+
     gameEmbed
+
 };
