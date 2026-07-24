823c2d8beab8fd698b9776695f3dfdf585a82511
piemcgamer338-stsfq <piemcgamer338@gmail.com>
2026-07-24 14:23:38 +0530

    Update roulette_col2.js

--- interactions/roulette_col2.js
+++ interactions/roulette_col2.js
@@ -1,13 +1,9 @@
-const rouletteHandler = require("./rouletteHandler");
+const { handleRoulette } = require("./rouletteHandler");
 
 module.exports = {
-
     customId: "roulette_col2",
 
     async execute(interaction) {
-
-        return rouletteHandler.execute(interaction);
-
+        return handleRoulette(interaction, "Column 2");
     }
-
 };
