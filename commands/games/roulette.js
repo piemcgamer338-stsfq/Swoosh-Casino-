8a91fe0c9095c308d0e0d44863a03239892a4214
piemcgamer338-stsfq <piemcgamer338@gmail.com>
2026-07-24 14:50:23 +0530

    Update roulette.js

--- commands/games/roulette.js
+++ commands/games/roulette.js
@@ -7,356 +7,140 @@ const {
 } = require("discord.js");
 
 const balanceService = require("../../services/balanceService");
-
-
-const rouletteGames = new Map();
-
-
+const { startRoulette } = require("../../utils/rouletteHandler");
 
 module.exports = {
 
     name: "roulette",
-
-    aliases: [
-        "r"
-    ],
-
-
+    aliases: ["r"],
 
     async execute(message, args) {
 
-
-
-        const bet =
-            Number(args[0]);
-
-
+        const bet = Number(args[0]);
 
         if (!bet || bet <= 0) {
-
-            return message.reply(
-                "❌ Usage: `.roulette <amount>`"
-            );
-
+            return message.reply("❌ Usage: `.roulette <amount>`");
         }
 
-
-
-        const hasBalance =
-            await balanceService.hasBalance(
-                message.author.id,
-                bet
-            );
-
-
-
-        if (!hasBalance) {
-
-            return message.reply(
-                "❌ You don't have enough balance."
-            );
-
-        }
-
-
-
-
-        const image =
-            new AttachmentBuilder(
-
-                "./assets/roulette.png",
-
-                {
-                    name:
-                    "roulette.png"
-                }
-
-            );
-
-
-
-
-
-        const embed =
-
-            new EmbedBuilder()
-
-            .setColor("#c0392b")
-
-            .setTitle(
-                "🎰 Roulette"
-            )
-
-            .setDescription(
-`
-# Place your bets
-
-💰 **Bet per button**
-${bet} Points
-
-
-Each selected option costs ${bet} Points.
-
-Choose your predictions:
-`
-            )
-
-            .setImage(
-                "attachment://roulette.png"
-            )
-
-            .setFooter({
-                text:
-                "Roulette"
-            });
-
-
-
-
-
-
-
-        const row1 =
-
-            new ActionRowBuilder()
-
-            .addComponents(
-
-
-                new ButtonBuilder()
-
-                .setCustomId(
-                    "roulette_col1"
-                )
-
-                .setLabel(
-                    "Column 1"
-                )
-
-                .setStyle(
-                    ButtonStyle.Danger
-                ),
-
-
-
-                new ButtonBuilder()
-
-                .setCustomId(
-                    "roulette_col2"
-                )
-
-                .setLabel(
-                    "Column 2"
-                )
-
-                .setStyle(
-                    ButtonStyle.Danger
-                ),
-
-
-
-                new ButtonBuilder()
-
-                .setCustomId(
-                    "roulette_col3"
-                )
-
-                .setLabel(
-                    "Column 3"
-                )
-
-                .setStyle(
-                    ButtonStyle.Danger
-                )
-
-            );
-
-
-
-
-
-
-
-        const row2 =
-
-            new ActionRowBuilder()
-
-            .addComponents(
-
-
-                new ButtonBuilder()
-
-                .setCustomId(
-                    "roulette_red"
-                )
-
-                .setLabel(
-                    "Red"
-                )
-
-                .setStyle(
-                    ButtonStyle.Danger
-                ),
-
-
-
-                new ButtonBuilder()
-
-                .setCustomId(
-                    "roulette_black"
-                )
-
-                .setLabel(
-                    "Black"
-                )
-
-                .setStyle(
-                    ButtonStyle.Secondary
-                ),
-
-
-
-                new ButtonBuilder()
-
-                .setCustomId(
-                    "roulette_odd"
-                )
-
-                .setLabel(
-                    "Odd"
-                )
-
-                .setStyle(
-                    ButtonStyle.Primary
-                )
-
-            );
-
-
-
-
-
-
-
-        const row3 =
-
-            new ActionRowBuilder()
-
-            .addComponents(
-
-
-                new ButtonBuilder()
-
-                .setCustomId(
-                    "roulette_even"
-                )
-
-                .setLabel(
-                    "Even"
-                )
-
-                .setStyle(
-                    ButtonStyle.Primary
-                ),
-
-
-
-                new ButtonBuilder()
-
-                .setCustomId(
-                    "roulette_low"
-                )
-
-                .setLabel(
-                    "1-16"
-                )
-
-                .setStyle(
-                    ButtonStyle.Success
-                ),
-
-
-
-                new ButtonBuilder()
-
-                .setCustomId(
-                    "roulette_high"
-                )
-
-                .setLabel(
-                    "17-36"
-                )
-
-                .setStyle(
-                    ButtonStyle.Success
-                )
-
-            );
-
-
-
-
-
-
-
-        const msg =
-            await message.reply({
-
-                embeds:[
-                    embed
-                ],
-
-                files:[
-                    image
-                ],
-
-                components:[
-
-                    row1,
-                    row2,
-                    row3
-
-                ]
-
-            });
-
-
-
-
-
-
-
-
-        rouletteGames.set(
-
-            msg.id,
-
-            {
-
-                userId:
-                message.author.id,
-
-
-                bet,
-
-
-                bets: [],
-
-
-                messageId:
-                msg.id
-
-            }
-
+        const hasBalance = await balanceService.hasBalance(
+            message.author.id,
+            bet
         );
 
+        if (!hasBalance) {
+            return message.reply("❌ You don't have enough balance.");
+        }
 
+        // Start roulette session
+        startRoulette(message.author.id, bet);
+
+        const image = new AttachmentBuilder(
+            "./assets/roulette.png",
+            {
+                name: "roulette.png"
+            }
+        );
+
+        const embed = new EmbedBuilder()
+            .setColor("#c0392b")
+            .setTitle("🎰 Roulette")
+            .setDescription(
+`# Place your bets
+
+💰 **Bet Per Selection**
+${bet} Points
+
+Each selected button costs **${bet}** Points.
+
+Choose as many bets as you want,
+then press **START**.`
+            )
+            .setImage("attachment://roulette.png")
+            .setFooter({
+                text: "Roulette"
+            });
+
+        const row1 = new ActionRowBuilder()
+            .addComponents(
+
+                new ButtonBuilder()
+                    .setCustomId("roulette_col1")
+                    .setLabel("Column 1")
+                    .setStyle(ButtonStyle.Danger),
+
+                new ButtonBuilder()
+                    .setCustomId("roulette_col2")
+                    .setLabel("Column 2")
+                    .setStyle(ButtonStyle.Danger),
+
+                new ButtonBuilder()
+                    .setCustomId("roulette_col3")
+                    .setLabel("Column 3")
+                    .setStyle(ButtonStyle.Danger)
+            );
+
+        const row2 = new ActionRowBuilder()
+            .addComponents(
+
+                new ButtonBuilder()
+                    .setCustomId("roulette_red")
+                    .setLabel("Red")
+                    .setStyle(ButtonStyle.Danger),
+
+                new ButtonBuilder()
+                    .setCustomId("roulette_black")
+                    .setLabel("Black")
+                    .setStyle(ButtonStyle.Secondary),
+
+                new ButtonBuilder()
+                    .setCustomId("roulette_odd")
+                    .setLabel("Odd")
+                    .setStyle(ButtonStyle.Primary)
+            );
+
+        const row3 = new ActionRowBuilder()
+            .addComponents(
+
+                new ButtonBuilder()
+                    .setCustomId("roulette_even")
+                    .setLabel("Even")
+                    .setStyle(ButtonStyle.Primary),
+
+                new ButtonBuilder()
+                    .setCustomId("roulette_low")
+                    .setLabel("1 - 16")
+                    .setStyle(ButtonStyle.Success),
+
+                new ButtonBuilder()
+                    .setCustomId("roulette_high")
+                    .setLabel("17 - 36")
+                    .setStyle(ButtonStyle.Success)
+            );
+
+        const row4 = new ActionRowBuilder()
+            .addComponents(
+
+                new ButtonBuilder()
+                    .setCustomId("roulette_start")
+                    .setLabel("🎰 START")
+                    .setStyle(ButtonStyle.Success)
+            );
+
+        await message.reply({
+
+            embeds: [embed],
+
+            files: [image],
+
+            components: [
+                row1,
+                row2,
+                row3,
+                row4
+            ]
+
+        });
 
     }
 
 };
-
-
-
-module.exports.rouletteGames =
-rouletteGames;
