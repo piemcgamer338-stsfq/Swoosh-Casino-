c1fa81955512422266e2ac46544bfdd88888fde3
piemcgamer338-stsfq <piemcgamer338@gmail.com>
2026-07-24 17:28:54 +0530

    Update blackjackLogic.js

--- utils/blackjackLogic.js
+++ utils/blackjackLogic.js
@@ -1,117 +1,249 @@
+const suits = [
+    "C",
+    "D",
+    "H",
+    "S"
+];
+
+
+const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+];
+
+
+
 function randomCard() {
 
-    const suits = ["♣", "♦", "♥", "♠"];
 
-    const values = [
-        "A",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "10",
-        "J",
-        "Q",
-        "K"
-    ];
+    const suit =
+        suits[
+            Math.floor(
+                Math.random() * suits.length
+            )
+        ];
+
+
+
+    const value =
+        values[
+            Math.floor(
+                Math.random() * values.length
+            )
+        ];
+
+
 
     return {
-        suit: suits[Math.floor(Math.random() * suits.length)],
-        value: values[Math.floor(Math.random() * values.length)]
+
+        suit,
+
+        value,
+
+        image:
+        `${suit}${value}.png`
+
     };
 
 }
 
+
+
+
+
 function handValue(hand) {
 
+
     let total = 0;
+
     let aces = 0;
+
+
 
     for (const card of hand) {
 
+
         if (card.value === "A") {
 
+
             total += 11;
+
             aces++;
 
-        } else if (["J", "Q", "K"].includes(card.value)) {
-
-            total += 10;
-
-        } else {
-
-            total += Number(card.value);
 
         }
 
+        else if (
+            ["J","Q","K"]
+            .includes(card.value)
+        ) {
+
+
+            total += 10;
+
+
+        }
+
+        else {
+
+
+            total += Number(card.value);
+
+
+        }
+
+
     }
 
-    while (total > 21 && aces > 0) {
+
+
+    while (
+        total > 21 &&
+        aces > 0
+    ) {
+
 
         total -= 10;
+
         aces--;
 
+
     }
+
+
 
     return total;
 
 }
 
+
+
+
+
 function isBlackjack(hand) {
 
-    return hand.length === 2 && handValue(hand) === 21;
+
+    return (
+
+        hand.length === 2 &&
+
+        handValue(hand) === 21
+
+    );
 
 }
 
+
+
+
+
 function isBust(hand) {
+
 
     return handValue(hand) > 21;
 
 }
 
+
+
+
+
 function dealerPlay(hand) {
 
-    while (handValue(hand) < 17) {
 
-        hand.push(randomCard());
+    while (
+        handValue(hand) < 17
+    ) {
+
+
+        hand.push(
+            randomCard()
+        );
+
 
     }
+
+
 
     return hand;
 
 }
 
-function getResult(player, dealer) {
 
-    const playerTotal = handValue(player);
-    const dealerTotal = handValue(dealer);
+
+
+
+function getResult(
+    player,
+    dealer
+) {
+
+
+    const playerTotal =
+        handValue(player);
+
+
+    const dealerTotal =
+        handValue(dealer);
+
+
 
     if (playerTotal > 21)
+
         return "lose";
+
+
 
     if (dealerTotal > 21)
+
         return "win";
+
+
 
     if (playerTotal > dealerTotal)
+
         return "win";
 
+
+
     if (dealerTotal > playerTotal)
+
         return "lose";
+
+
 
     return "push";
 
 }
 
+
+
+
+
 module.exports = {
 
+
     randomCard,
+
     handValue,
+
     isBlackjack,
+
     isBust,
+
     dealerPlay,
+
     getResult
 
 };
